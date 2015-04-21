4288c4a1367a8fbbbbbcc52a82869e271969cdc4
sredl <office@symena.com>
2015-04-21 12:13:24 +0200

    Build 32- and 64-bit versions of GDAL.

--- build.bat
+++ build.bat
@@ -1,14 +1,30 @@
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\vcvars64.bat"
 set repodir=%cd%
 cd gdal
-@echo Building debug variant
-nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 WIN64=YES GDAL_HOME=%repodir%\buildd
-nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 WIN64=YES GDAL_HOME=%repodir%\buildd devinstall
-copy gdal200.pdb ..\buildd\bin\
-nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 WIN64=YES GDAL_HOME=%repodir%\buildd clean
 
-@echo Building release variant
-nmake -f makefile.vc MSVC_VER=1800 WITH_PDB=1 WIN64=YES GDAL_HOME=%repodir%\buildo
-nmake -f makefile.vc MSVC_VER=1800 WITH_PDB=1 WIN64=YES GDAL_HOME=%repodir%\buildo devinstall
-copy gdal200.pdb ..\buildo\bin\
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\vcvars64.bat"
+
+@echo Building 64-bit debug variant
+nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 WIN64=YES GDAL_HOME=%repodir%\buildd64
+nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 WIN64=YES GDAL_HOME=%repodir%\buildd64 devinstall
+copy gdal200.pdb ..\buildd64\bin\
+nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 WIN64=YES GDAL_HOME=%repodir%\buildd64 clean
+
+@echo Building 64-bit release variant
+nmake -f makefile.vc MSVC_VER=1800 WITH_PDB=1 WIN64=YES GDAL_HOME=%repodir%\buildo64
+nmake -f makefile.vc MSVC_VER=1800 WITH_PDB=1 WIN64=YES GDAL_HOME=%repodir%\buildo64 devinstall
+copy gdal200.pdb ..\buildo64\bin\
+nmake -f makefile.vc MSVC_VER=1800 WITH_PDB=1 WIN64=YES GDAL_HOME=%repodir%\buildo64 clean
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64_x86\vcvarsamd64_x86.bat"
+
+@echo Building 32-bit debug variant
+nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 GDAL_HOME=%repodir%\buildd32
+nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 GDAL_HOME=%repodir%\buildd32 devinstall
+copy gdal200.pdb ..\buildd32\bin\
+nmake -f makefile.vc MSVC_VER=1800 DEBUG=1 GDAL_HOME=%repodir%\buildd32 clean
+
+@echo Building 32-bit release variant
+nmake -f makefile.vc MSVC_VER=1800 WITH_PDB=1 GDAL_HOME=%repodir%\buildo32
+nmake -f makefile.vc MSVC_VER=1800 WITH_PDB=1 GDAL_HOME=%repodir%\buildo32 devinstall
+copy gdal200.pdb ..\buildo32\bin\
