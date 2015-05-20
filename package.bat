79c02dc767f5cd54908471d3a9fb42e1d6bc73af
jinfuehr <office@symena.com>
2015-05-20 08:51:56 +0200

    Fix version number from pipeline count and fall back to 0 if variables are not set

--- package.bat
+++ package.bat
@@ -1,3 +1,5 @@
+if not defined GO_STAGE_COUNTER (set GO_STAGE_COUNTER=0)
+if not defined GO_PIPELINE_COUNTER (set GO_PIPELINE_COUNTER=0)
 
 @echo Building NuGet packages
 PowerShell.exe -Command Write-NuGetPackage -Verbose .\gdal.autopkg
