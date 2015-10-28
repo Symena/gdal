6c816bb03df5ca6dc12503cec0d5309018346bbd
jinfuehr <office@symena.com>
2015-10-28 14:57:47 +0100

    Make indexMain compilable again, refs #7235 #7243

--- gdal/frmts/index/indexMain/indexMain.cpp
+++ gdal/frmts/index/indexMain/indexMain.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-	IndexDataset i;
+// 	GDALOpenInfo openInfo(R"(Q:\Data\Scenarios\Work\Asset8.1\Houston\mapdata\Height\Houston\index.txt)", GA_ReadOnly);
+// 
+// 	IndexDataset::Open(&openInfo);
 
 	return 0;
 }
