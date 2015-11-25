7134c467c7e0c8ff888dc688bd192f36e1030b00
gerd.katzenbeisser@teoco.com <gerd.katzenbeisser@teoco.com>
2015-11-25 17:16:50 +0100

    Disable cache when installing gdal-symena nuget package. refs #7235

--- test.bat
+++ test.bat
@@ -10,7 +10,7 @@ cd gdal\frmts\aircom_map
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" || goto :error
 
 @echo Installing NuGet packages
-nuget install gdal-symena -SolutionDirectory . -Source "%cd%\..\..\.." || goto :error
+nuget install gdal-symena -SolutionDirectory . -NoCache -Source "%cd%\..\..\.." || goto :error
 nuget restore AircomMap.sln || goto :error
 
 @echo Building whole solution incl. tests (64-bit release)
