52072b6552ad127e784dfb78229624bf195c64a3
mkinkelin <office@symena.com>
2016-04-08 12:53:46 +0200

    RasterBand: determine block (tile) size and number of blocks, refs #7429

--- gdal/frmts/aircom/AircomPredRaster/RasterBand.cpp
+++ gdal/frmts/aircom/AircomPredRaster/RasterBand.cpp
@@ -11,6 +11,11 @@ RasterBand::RasterBand(Dataset* owningDataSet, int bandIndex, int section)
 
 	// nRasterXSize & nRasterYSize are set to the dataset size by GDALDataset::SetBand()
 
+	// the size of each block and the number of blocks will be lazily initialized via
+	// initializeBlocksInfo() to avoid unnecessary API accesses
+	nBlockXSize = nBlockYSize = -1;
+	nBlocksPerRow = nBlocksPerColumn = -1;
+
 	SetNoDataValue(-9999);
 }
 
@@ -19,9 +24,34 @@ GDALColorInterp RasterBand::GetColorInterpretation()
 	return GDALColorInterp::GCI_GrayIndex;
 }
 
+CPLErr RasterBand::IRasterIO(GDALRWFlag eRWFlag, int nXOff, int nYOff, int nXSize, int nYSize,
+	void * pData, int nBufXSize, int nBufYSize, GDALDataType eBufType, GSpacing nPixelSpace,
+	GSpacing nLineSpace, GDALRasterIOExtraArg* psExtraArg)
+{
+	if (nBlockXSize == -1)
+		initializeBlocksInfo();
+
+	return GDALPamRasterBand::IRasterIO(eRWFlag, nXOff, nYOff, nXSize, nYSize, pData,
+		nBufXSize, nBufYSize, eBufType, nPixelSpace, nLineSpace, psExtraArg);
+}
+
 CPLErr RasterBand::IReadBlock(int, int, void*)
 {
 	return CPLErr::CE_Failure;
+}
+
+void RasterBand::initializeBlocksInfo()
+{
+	auto firstTile = getPredRaster()->CreateTileIterator(section)->GetNextTile();
+
+	_REGIONEX tileRegion;
+	firstTile->GetTileRegion(&tileRegion);
+
+	nBlockXSize = tileRegion.m_width;
+	nBlockYSize = tileRegion.m_height;
+
+	nBlocksPerRow = (nRasterXSize + nBlockXSize - 1) / nBlockXSize;    // ceiling(nRasterXSize / nBlockXSize)
+	nBlocksPerColumn = (nRasterYSize + nBlockYSize - 1) / nBlockYSize; // ceiling(nRasterYSize / nBlockYSize)
 }
 
 }}
--- gdal/frmts/aircom/AircomPredRaster/RasterBand.h
+++ gdal/frmts/aircom/AircomPredRaster/RasterBand.h
@@ -8,17 +8,25 @@ namespace aircom { namespace pred_raster {
 
 class RasterBand: public GDALPamRasterBand
 {
-	int section;
+	const int section;
+
 public:
 	RasterBand(Dataset* owningDataSet, int bandIndex, int section);
-	
+
 	virtual GDALColorInterp GetColorInterpretation() override;
 
 protected:
+	virtual CPLErr IRasterIO(GDALRWFlag eRWFlag, int nXOff, int nYOff, int nXSize, int nYSize,
+		void* pData, int nBufXSize, int nBufYSize, GDALDataType eBufType,
+		GSpacing nPixelSpace, GSpacing nLineSpace, GDALRasterIOExtraArg* psExtraArg) override;
+
 	virtual CPLErr IReadBlock(int nXBlockOff, int nYBlockOff, void* pImage) override;
 
 private:
 	Dataset& getDataset() { return *static_cast<Dataset*>(poDS); }
+	IPredRaster5Ptr getPredRaster() { return getDataset().getPredRaster(); }
+
+	void initializeBlocksInfo();
 };
 
 }}
