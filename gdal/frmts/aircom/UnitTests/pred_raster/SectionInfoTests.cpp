26175764711bc5d6146a7632abc5fa096d7ab93e
gerd.katzenbeisser@teoco.com <gerd.katzenbeisser@teoco.com>
2016-04-12 10:01:15 +0200

    Transform GeoParamsTests to SectionInfoTests. refs #7429

--- gdal/frmts/aircom/UnitTests/pred_raster/SectionInfoTests.cpp
+++ gdal/frmts/aircom/UnitTests/pred_raster/SectionInfoTests.cpp
@@ -3,40 +3,42 @@
 #include <gmock/gmock.h>
 
 #include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 #include <sstream>
 
 namespace aircom { namespace pred_raster {
 
 using boost::property_tree::wptree;
 
-struct GeoParamsTests : public testing::Test
+struct SectionInfoTests : public testing::Test
 {
-	wptree geoNode;
+	wptree sectionNode;
 
-	GeoParamsTests()
+	SectionInfoTests()
 	{
-		geoNode.add<int>(L"left", 1);
-		geoNode.add<int>(L"right", 2);
-		geoNode.add<int>(L"bottom", 3);
-		geoNode.add<int>(L"top", 4);
+		std::wstring sectionJson = LR"({
+			"bottomLeft": [1, 3],
+			"topRight": [2, 4],
+			"dataType": "R64",
+			"tileSizeInPixels": [5, 6] })";
 
+		std::wstringstream stream;
+		stream << sectionJson;
+		boost::property_tree::json_parser::read_json(stream, sectionNode);
 	}
 };
 
-TEST_F(GeoParamsTests, ParsesBounds)
+TEST_F(SectionInfoTests, ParsesBounds)
 {
-	GeoParams geoParams(geoNode);
+	SectionInfo sectionInfo(sectionNode);
 
 	MapPoint bottomLeft(1, 3);
 	MapPoint topRight(2, 4);
-	MapBox expected(bottomLeft, topRight);
+	MapBox bounds(bottomLeft, topRight);
 
-	EXPECT_EQ(expected, geoParams.boundingBox);
-}
+	SectionInfo expected(bounds, GDALDataType::GDT_Float64, {5, 6});
 
-TEST_F(GeoParamsTests, ParsesProjection)
-{
-	// TODO
+	EXPECT_EQ(expected, sectionInfo);
 }
 
 }}
